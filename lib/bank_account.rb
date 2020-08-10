ed29f13e7052200d763b7b5e9e0ab48c34d40f00
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-08-10 23:01:31 +0100

    added show statment method and test

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,17 +1,32 @@
+require'Date'
 class BankAccount
   attr_reader :balance
   def initialize
     @balance = 0
+    @array = []
   end
 
 
 def deposit(m)
   @balance += m
+  @array << [Date.today.strftime('%d/%m/%Y'),'%.2f'% m, nil ,'%.2f'% @balance]
 end
 
 
-def withdraw(m)
-  raise 'exceeded balance' if m > @balance 
-  @balance -= m
+def withdraw(c)
+  raise 'exceeded balance' if c > @balance
+  @balance -= c
+  @array << [Date.today.strftime('%d/%m/%Y'), nil,'%.2f'% c,'%.2f'% @balance]
+
+end
+
+
+def show_statment
+  lines = ["date || credit || debit || balance"
+  ]
+@array.reverse.each do |n| lines << n.join(" || ").gsub!("  "," ")
+end
+lines.join("\n")
+
 end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -21,4 +21,17 @@ describe '#withdraw' do
   end
 
 end
+
+describe '#show_statment'do
+it' shows the date, deposit, withdraw and balance' do
+  account.deposit(1000)
+  account.deposit(2000)
+  account.withdraw(500)
+
+  expect(account.show_statment).to eq("date || credit || debit || balance
+10/08/2020 || || 500.00 || 2500.00
+10/08/2020 || 2000.00 || || 3000.00
+10/08/2020 || 1000.00 || || 1000.00")
+end
+end
 end
