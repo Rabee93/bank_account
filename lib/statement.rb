290c7f7a33820982a37725ccf0eb0e39050c1109
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-10-19 20:38:30 +0100

    refactored

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,16 +1,16 @@
 
- class Statement
+class Statement
 
-   def show_statement(transactions)
-     lines = ['date || credit || debit || balance']
-     transactions.reverse.each do |n|
-       line = [n.date.strftime('%d/%m/%Y'),
-         n.credit ? format('%.2f', n.credit) : nil,
-         n.debit ? format('%.2f', n.debit) : nil,
-         format('%.2f', n.balance)
-        ]
-       lines << line.join(' || ').gsub!('  ', ' ')
-     end
-     lines.join("\n")
-   end
- end
+  def show_statement(transactions)
+    lines = ['date || credit || debit || balance']
+    transactions.reverse.each do |n|
+      line = [n.date.strftime('%d/%m/%Y'),
+        n.credit ? format('%.2f', n.credit) : nil,
+        n.debit ? format('%.2f', n.debit) : nil,
+        format('%.2f', n.balance)
+      ]
+      lines << line.join(' || ').gsub!('  ', ' ')
+    end
+    lines.join("\n")
+  end
+end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,8 +2,8 @@ require 'statement'
 
 
 describe Statement do
-let(:account) { BankAccount.new }
-let(:statement) { Statement.new }
+  let(:account) { BankAccount.new }
+  let(:statement) { Statement.new }
   describe '#show_statement' do
     it ' shows the date, deposit, withdraw and balance' do
       allow(Date).to receive(:today) { Date.new(2020, 8, 10) }
@@ -11,10 +11,11 @@ let(:statement) { Statement.new }
       account.deposit(2000)
       account.withdraw(500)
 
-    expect(statement.show_statement(account.transactions)).to eq("date || credit || debit || balance
-10/08/2020 || || 500.00 || 2500.00
-10/08/2020 || 2000.00 || || 3000.00
-10/08/2020 || 1000.00 || || 1000.00")
+      expect(statement.show_statement(account.transactions)).to eq(
+        "date || credit || debit || balance\n" +
+        + "10/08/2020 || || 500.00 || 2500.00\n" +
+        + "10/08/2020 || 2000.00 || || 3000.00\n" +
+        + "10/08/2020 || 1000.00 || || 1000.00")
+      end
+    end
   end
-end
-end
